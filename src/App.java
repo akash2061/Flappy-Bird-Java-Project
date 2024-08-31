34d179df17f24e2000963c704580597dc6712f13
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-31 21:06:34 +0530

    Create Frame

--- src/App.java
+++ src/App.java
@@ -6,10 +6,15 @@ public class App {
         int bHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+        // frame.setVisible(true);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(bWigth, bHeight);
+        frame.setLocationRelativeTo(null);
+
+        FlappyBird f = new FlappyBird();
+        frame.add(f);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
